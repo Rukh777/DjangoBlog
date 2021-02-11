28780c8027d715c01ea29d8a0f2752f0e4dd523d
Rukhsar Khan <rukhsarmev07@gmail.com>
2021-02-11 09:01:03 +0530

    rm from url

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from blog import views
 
-from django.views.static import serve
-from djnago.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,7 +34,7 @@ urlpatterns = [
     path('deletepost/<int:id>', views.delete_post, name='deletepost'),
 
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
